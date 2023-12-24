b62214d2614d6b0aa75db77beb0e960422994042
Dương Song <duongsong9a4@gmail.com>
2023-12-24 14:51:33 +0700

    first commit

---
+++ src/main/java/com/example/devops20127310/Devops20127310Application.java
@@ -0,0 +1,13 @@
+package com.example.devops20127310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops20127310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops20127310Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops20127310/Devops20127310ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops20127310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops20127310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
